63eb2e97bbada106e62e8603f409dbcded2d0088
Urso-Suzi <pedroviniciussouza24@gmail.com>
2023-12-19 05:46:38 -0300

    adicionando validacao de palavra

---
+++ JavaScript/funçõesJS/epar.js
@@ -0,0 +1,10 @@
+
+let palavra = 'Olá joão'
+
+function temOla(palavra) {
+    if(palavra.includes('Olá')){
+        console.log('Tem olá!');
+    } else {console.log('Não tem ola')}
+}
+
+temOla(palavra);
