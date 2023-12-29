b008c5f9bc788a407d52edb9917dfc039d805125
Urso-Suzi <pedroviniciussouza24@gmail.com>
2023-12-29 05:00:27 -0300

    aprendendo a criar a minha propria tag html

--- WebComponents/desafio-01/src/components/CardNews.js
+++ WebComponents/desafio-01/src/components/CardNews.js
@@ -1,12 +1,71 @@
+// class CardNews extends HTMLElement {
+//     constructor () {
+//         super();
+
+//         const shadow = this.attachShadow({mode: "open"});
+//         shadow.innerHTML = "<h1>Hello Wolrd</h1>"
+//     }
+// }
+
+
+
+// customElements.define('card-news', CardNews)
+
+
 class CardNews extends HTMLElement {
-    constructor () {
+    constructor() {
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello Wolrd</h1>"
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.style());
     }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+        //elementos do cardLeft
+        const autor = document.createElement("span");
+        const linkTitle = document.createElement("a");
+        const newsContent = document.createElement("p");
+
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+
+
+
+
+
+        const cardRigth = document.createElement("div");
+        cardLeft.setAttribute("class", "card__rigth");
+        //elementos do cardRigth
+        const newsImage = document.createElement("img");
+
+
+        cardRigth.appendChild(newsImage);
+
+
+
+
+
+
+
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRigth);
+
+        return componentRoot;
+    }
+
+    styles() {}
+    
+
 }
 
-
-
-customElements.define('card-news', CardNews)
+customElements.define("card-news", CardNews)
