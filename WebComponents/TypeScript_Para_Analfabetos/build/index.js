312b2eb2b74bf29d3b7e7176d6db48b9d058dfa4
Urso-Suzi <pedroviniciussouza24@gmail.com>
2024-01-01 20:11:37 -0300

    aprendendo typescrpt

---
+++ WebComponents/TypeScript_Para_Analfabetos/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function printaObjetos(pessoa) {
+    console.log(pessoa);
+}
+printaObjetos({
+    name: "Pedro",
+    vulgo: "Jefferson",
+});
+console.log("Olá mundo");
---
+++ WebComponents/TypeScript_Para_Analfabetos/src/index.ts
@@ -0,0 +1,19 @@
+type heroi = {
+    name: string;
+    vulgo: string;
+};
+
+
+
+
+function printaObjetos(pessoa: heroi) {
+    console.log(pessoa);
+}
+
+printaObjetos({
+    name: "Pedro",
+    vulgo: "Jefferson",
+});
+
+
+console.log("Olá mundo");
