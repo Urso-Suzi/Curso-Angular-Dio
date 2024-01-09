55da32f4094751072ba897a54f993767a3d9d792
Urso-Suzi <pedroviniciussouza24@gmail.com>
2024-01-09 05:57:55 -0300

    aprendendo angular

--- Angular/projeto1/src/main.ts
+++ Angular/projeto1/src/main.ts
@@ -1,6 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {MenuComponent} from './app/components/menu.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports:[BrowserModule, FormsModule],
+  declarations: [AppComponent, MenuComponent],
+
+  bootstrap: [AppComponent],
+})
+
+export class AppModule {}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+ 
+
