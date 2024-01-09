d9b467ca55e67f913e2197013232a69c58342147
Urso-Suzi <pedroviniciussouza24@gmail.com>
2024-01-09 13:01:03 -0300

    criando componentes em angular

--- Angular/projeto2_versao14/src/app/app.module.ts
+++ Angular/projeto2_versao14/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu.component';
+import { Button } from './components/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    Button
   ],
   imports: [
     BrowserModule,
---
+++ Angular/projeto2_versao14/src/app/components/button.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component ({
+    selector: 'my-button',
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.css'],
+})
+
+export class Button{
+    buttonText:string = 'ACESSAR';
+    buttonTexts:string[] = ['VENDER', 'COMPRAR']
+    buttonNumber: number = 1;
+    buttonObject = {
+        label: 'adicionar um caminho',
+    }
+}
