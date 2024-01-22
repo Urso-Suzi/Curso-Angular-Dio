bc56c3a4153f55b40243e6aa2323b00ea49b2f00
Urso-Suzi <pedroviniciussouza24@gmail.com>
2024-01-22 13:59:29 -0300

    entendendo check

--- Angular/Projetocurso1/life-style/src/app/app.component.ts
+++ Angular/Projetocurso1/life-style/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'life-style';
+  isAliveCheckSAmple:boolean = true;
+
+
+  disposeCheckSample():void {
+    this.isAliveCheckSAmple = false;
+  }
 }
--- Angular/Projetocurso1/life-style/src/app/check-sample/check-sample.component.ts
+++ Angular/Projetocurso1/life-style/src/app/check-sample/check-sample.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+OnInit, 
+DoCheck, 
+AfterContentChecked,
+AfterViewChecked,
+AfterViewInit,
+OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-check-sample',
   templateUrl: './check-sample.component.html',
   styleUrls: ['./check-sample.component.css']
 })
-export class CheckSampleComponent implements OnInit {
+export default class CheckSampleComponent implements OnInit, 
+DoCheck, 
+AfterContentChecked,
+AfterViewChecked,
+AfterViewInit,
+OnDestroy {
+
+  quantidade:number = 0;
 
   constructor() { }
+  ngOnDestroy(): void {
+    console.log("Good bye, my friend.")
+  }
+
+  adicionar() {
+    this.quantidade += 1;
+    console.log(this.quantidade)
+  }
+
+  decrementar() {
+    this.quantidade -= 1;
+    console.log(this.quantidade)
+  }
+
+  // quando o primeiro conteúdo é iniciado
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+
+  // depois da inicialização da view
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+
+  // após alguma alteração, verifica o conteúdo
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked")
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDocheck")
+  }
 
   ngOnInit(): void {
+    console.log("ngOnInit")
   }
 
 }
