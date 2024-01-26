8f824d0a6eaa39a7cac574457e20e29a82d26326
Urso-Suzi <pedroviniciussouza24@gmail.com>
2024-01-26 12:56:47 -0300

    aprendendo ng models, com biding e criação com ngfor

--- Angular/Diretivas-project/diretiva_projects/src/app/app.module.ts
+++ Angular/Diretivas-project/diretiva_projects/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CompAtributosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
